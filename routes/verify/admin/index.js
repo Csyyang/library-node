6bebe79cfe222878f1b30988d64ef336d51019b5
csy <693765678@qq.com>
2023-09-02 17:39:08 +0800

    读者增删改查

--- routes/verify/admin/index.js
+++ routes/verify/admin/index.js
@@ -3,8 +3,12 @@ const router = express.Router();
 const conn = require('../../../mysql/index')
 
 const book = require('./book')
+const reader = require('./reader')
 
 // 书籍管理
-router.use('/',book)
+router.use('/book', book)
+
+// 读者管理
+router.use('/reader', reader)
 
 module.exports = router
