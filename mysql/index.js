a1104f9419e932316aa6b67764a3d3d2a6a72791
csy <693765678@qq.com>
2023-09-02 12:50:20 +0800

    mysql自动断开处理

--- mysql/index.js
+++ mysql/index.js
@@ -1,15 +1,32 @@
 const mysql = require('mysql');
 
-const conn = mysql.createConnection({
+const sqlConfig = {
     host: 'localhost',
     user: 'root',
     password: 'root',
     database: 'library'
-})
-// 连接数据库
-conn.connect(function (err) {
-    if (err) { console.log("连接失败") };
-    console.log("连接成功,当前连接线程ID" + conn.threadId);
-})
+}
+let conn
+function handleDisconnect() {
+    conn = mysql.createConnection(sqlConfig)
+    // 连接数据库
+    conn.connect(function (err) {
+        if (err) { console.log("连接失败") };
+        console.log("连接成功,当前连接线程ID" + conn.threadId);
+    })
+
+    conn.on('error', function (err) {
+        console.log('db error', err);
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+            handleDisconnect();                         // lost due to either server restart, or a
+        } else {                                      // connnection idle timeout (the wait_timeout
+            throw err;                                  // server variable configures this)
+        }
+    });
+}
+
+handleDisconnect();
+
+
 //导出
 module.exports = conn;
