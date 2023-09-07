9f8381b7bb1337fd4d996a36000329887ee00df7
csy23333 <693765678@qq.com>
2023-09-07 17:10:42 +0800

    图书改造

--- routes/verify/admin/book/index.js
+++ routes/verify/admin/book/index.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const router = express.Router();
-// const conn = require('../../../../mysql/index');
-// const queryAsync = require('../../../../mysql/queryAsync');
 const conn = require('../../../../mysql/promiseSql')
 const { body, query, validationResult } = require('express-validator');
 const response = require("../../../../util/response");
@@ -24,11 +22,11 @@ router.post('/addBook', [
     }
 
     const body = req.body
-    const sql = `INSERT INTO book (title, author, publisher, publication_date, isbn, category_id, copies_available) VALUES (?,?,?,?,?,?,?)`;
+    const sql = `INSERT INTO books (title, author, publisher, publication_date, isbn, category_id, copies_available, copies_total) VALUES (?,?,?,?,?,?,?,?)`;
 
 
     try {
-        conn.query(sql, [body.title, body.author, body.publisher, body.publication_date, body.isbn, body.category_id, body.copies_total])
+        await conn.query(sql, [body.title, body.author, body.publisher, body.publication_date, body.isbn, body.category_id, body.copies_available, body.copies_total])
         response({}, res)
     } catch (error) {
         console.log(error);
@@ -44,26 +42,24 @@ router.post('/addBook', [
 // 删除
 router.get('/delBook', [
     query('book_id').notEmpty().withMessage('book_id不能为空'),
-], (req, res) => {
+], async (req, res) => {
     // 参数校验
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return response(errors.array(), res, '01')
     }
-    const sql = `DELETE FROM book WHERE book_id = ${req.query.book_id};`
-
-    conn.query(sql, function (err, result) {
-        console.log(result)
-        if (err) {
-            console.log(err);
-            response(err, res, '01')
-            return
-        }
+    const sql = `DELETE FROM books WHERE book_id = ${req.query.book_id};`
+    try {
+        const [result] = await conn.query(sql)
         if (result.affectedRows === 0) {
             return response('未找到图书', res, '01')
         }
         response({}, res)
-    })
+    } catch (err) {
+        console.log(err);
+        response(err, res, '01')
+        return
+    }
 })
 
 // 更新书籍
@@ -74,10 +70,10 @@ router.post('/updateBook', [
     body('publication_date').notEmpty().isDate().withMessage('出版日期格式不正确'),
     body('isbn').notEmpty().withMessage('国际标准书号不能为空'),
     body('category_id').notEmpty().withMessage('图书分类不能为空'),
-    body('description').notEmpty().withMessage('书籍描述不能为空'),
-    body('status').notEmpty().withMessage('状态不能为空'),
+    body('copies_available').notEmpty().withMessage('可接数量不能为空'),
+    body('copies_total').notEmpty().withMessage('图书总量不能为空'),
     body('book_id').notEmpty().withMessage('book_id不能为空')
-], (req, res) => {
+], async (req, res) => {
     // 参数校验
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -86,16 +82,17 @@ router.post('/updateBook', [
 
 
     const body = req.body
-    const sql = `UPDATE book SET title = '${body.title}', author = '${body.author}',publisher = '${body.publisher}', publication_date = '${body.publication_date}', isbn = '${body.isbn}', category_id = '${body.category_id}',
-     description = '${body.description}', status = '${body.status}' WHERE book_id = ${body.book_id};`
-    conn.query(sql, function (err, result) {
-        if (err) {
-            console.log(err);
-            response(err, res, '01')
-            return
-        }
+    const sql = `UPDATE books SET title = '${body.title}', author = '${body.author}',publisher = '${body.publisher}', publication_date = '${body.publication_date}', isbn = '${body.isbn}',call_number = '${body.call_number || ''}', category_id = '${body.category_id}',
+    copies_available = '${body.copies_available}', copies_total = '${body.copies_total}' WHERE book_id = ${body.book_id};`
+
+    try {
+        await conn.query(sql)
         response({}, res)
-    })
+    } catch (err) {
+        console.log(err);
+        response(err, res, '01')
+        return
+    }
 })
 
 // 获取所有图书
@@ -113,7 +110,7 @@ router.get('/allBook', [
     // --获取总记录数
     const sqlAll = `SELECT COUNT(*) as 'all' FROM books;`
     try {
-        const all = await conn(sqlAll)
+        const all = await conn.query(sqlAll)
         console.log(all)
         resObj.all = all[0].all
     } catch (error) {
@@ -123,9 +120,13 @@ router.get('/allBook', [
     }
 
     try {
-        const sql = `SELECT *, DATE_FORMAT(publication_date, '%Y-%m-%d') AS publication_date FROM books LIMIT ${req.query.size} OFFSET ${req.query.page - 1};`
-        const sqlList = await conn(sql)
-
+        const sql = `SELECT books.*, DATE_FORMAT(publication_date, '%Y-%m-%d') AS publication_date, book_categories.category_name
+        FROM books
+        INNER JOIN book_categories ON books.category_id = book_categories.category_id
+        LIMIT ? OFFSET ?`
+        const page = req.query.page;
+        const size = req.query.size;
+        const [sqlList] = await conn.query(sql, [parseInt(size), parseInt(page - 1)]);
         resObj.list = sqlList
         response(resObj, res)
     } catch (error) {
