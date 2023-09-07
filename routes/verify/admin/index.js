e9544f25e3aec3d911ed2b06f4ced82ee70db4f6
csy23333 <693765678@qq.com>
2023-09-07 16:26:29 +0800

    分类

--- routes/verify/admin/index.js
+++ routes/verify/admin/index.js
@@ -4,6 +4,7 @@ const conn = require('../../../mysql/index')
 
 const book = require('./book')
 const reader = require('./reader')
+const category = require('./category')
 
 // 书籍管理
 router.use('/book', book)
@@ -11,4 +12,6 @@ router.use('/book', book)
 // 读者管理
 router.use('/reader', reader)
 
+
+router.use('/category', category)
 module.exports = router
